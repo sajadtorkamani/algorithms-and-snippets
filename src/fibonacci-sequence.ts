9ed246b0170ba03977ec2b8137dced4956d6f15d
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-08-14 13:31:17 +0100

    Initial commit

---
+++ src/fibonacci-sequence.ts
@@ -0,0 +1,23 @@
+export function fibbonaciSequence(num: number): number[] {
+  const sequence = [0, 1]
+
+  // Validate input
+  if (num <= 0) {
+    throw new Error(`Invalid input: ${num}. Must be positive integer`)
+  }
+
+  if (num === 1) {
+    return [0]
+  }
+
+  // Generate sequence
+  for (let sequenceIndex = 2; sequenceIndex < num; sequenceIndex++) {
+    const previousNum = sequence[sequenceIndex - 1]
+    const precedingNum = sequence[sequenceIndex - 2]
+    const newSequenceNum = previousNum + precedingNum
+
+    sequence.push(newSequenceNum)
+  }
+
+  return sequence
+}
