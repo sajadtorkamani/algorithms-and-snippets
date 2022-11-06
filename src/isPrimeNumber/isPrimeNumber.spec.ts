0732491537b2085bc9ae0d289546515609ef4802
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-11-06 14:46:18 +0000

    WIP

---
+++ src/isPrimeNumber/isPrimeNumber.spec.ts
@@ -0,0 +1,17 @@
+import { isPrimeNumber } from './isPrimeNumber'
+
+describe('isPrimeNumber', () => {
+  test.each([2, 3, 5, 7, 11, 17, 19, 23])(
+    'returns true if number is a prime',
+    (num) => {
+      expect(isPrimeNumber(num)).toBe(true)
+    }
+  )
+
+  test.each([4, 6, 8, 9, 10, 12, 14])(
+    'returns false if number is not a prime',
+    (num) => {
+      expect(isPrimeNumber(num)).toBe(false)
+    }
+  )
+})
---
+++ src/isPrimeNumber/isPrimeNumber.ts
@@ -0,0 +1,16 @@
+// A prime number is a number that's only divisible by itself and 1
+export function isPrimeNumber(num: number): boolean {
+  if (num <= 1) {
+    return false
+  }
+
+  for (let n = 2; n < num; n++) {
+    // If the number is divisisble by any number other than itself and 1,
+    // it's not a prime so return false
+    if (num % n === 0) {
+      return false
+    }
+  }
+
+  return true
+}
