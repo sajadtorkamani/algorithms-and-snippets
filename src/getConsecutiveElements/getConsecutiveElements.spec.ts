e2e97ffa46010bd0f992004927909f51d8c51f81
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-11-06 16:07:53 +0000

    WIP

--- src/getConsecutiveElements/getConsecutiveElements.spec.ts
+++ src/getConsecutiveElements/getConsecutiveElements.spec.ts
@@ -1,6 +1,6 @@
 import { getConsecutiveElements } from './getConsecutiveElements'
 
-describe('getConsecutiveNums', () => {
+describe('getConsecutiveElements', () => {
   test('returns array of consecutive elements', () => {
     expect(getConsecutiveElements([2, 2, 4, 5, 6, 2, 2, 2, 7, 2], 2)).toEqual([
       [2, 2],
